59b88f8414eede3f21370c37d34d176aefad9cb5
David <david.polania3@gmail.com>
2021-08-12 16:25:04 -0500

    Ex 13 Recursion

--- Exercises/Ex13-Recursion/main.go
+++ Exercises/Ex13-Recursion/main.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main()  {
-	
+	exponencia(2)
 }
-
+//Recursion es una funcion que se llama a si misma
 func exponencia(num int)  {
-	
+	if num > 3000{
+		return
+	}
+	fmt.Println(num)
+	exponencia(num * 2)
 }
